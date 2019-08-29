6ee0beebceec162e9b2f7f8f92030e547b01bfc0
student <student@pop-os.localdomain>
2019-08-29 13:57:50 +1200

    create buzzes table

---
+++ server/migrations/20190829115748_buzzes.js
@@ -0,0 +1,14 @@
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('buzzes', (table) => {
+        table.increments('id').primary()
+        table.string('content')
+        table.integer('hive_id')
+        table.integer('bee_id')
+        table.date('end-date')
+        //table.array('plusone')
+    })
+};
+
+exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('hives')
+};
