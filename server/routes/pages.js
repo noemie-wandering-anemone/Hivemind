c59692861988ef35572e4493efc06fa32a1a035a
Noemie <wandering_aneone@protonmail.com>
2019-08-31 22:05:22 +1200

    Create ask get route

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -8,4 +8,13 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.post('/', (req, res) => {
+  res.redirect('/ask')
+})
+
+router.get('/ask', (req, res) => {
+  res.render('ask')
+})
+
+
 module.exports = router
