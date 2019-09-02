42ca94f38cec24674648da8e3700771b0fbdff3f
Noemie <wandering_aneone@protonmail.com>
2019-09-02 22:14:23 +1200

    Calculate end date

--- server/db.js
+++ server/db.js
@@ -2,19 +2,28 @@ const config = require('../knexfile').development
 const connection = require('knex')(config)
 const moment = require('moment');
 
+function getHive(id, db = connection) {
+  return db('hives')
+    .where('id', id).first()
+}
+
 function createHive (hive, db = connection) {
   return db('hives')
     .insert({
       'subject': hive.question,
       'description': hive.explanation,
-      'date_created': moment().format('MMMM Do YYYY, h:mm:ss a')
+      'dateCreated': moment().format('Do MMMM YYYY'),
+      'endDate': endDate(hive.duration),
     })
 }
 
-function getHive(id, db = connection) {
-  return db('hives')
-    .where('id', id).first()
+function endDate(duration) {
+  let startDate = moment().format('Do MMMM YYYY')
+  let num = Number(duration)
+  let endDate = moment(startDate, 'Do MMMM YYYY').add(num, 'days')
+  return endDate.format('MMMM Do YYYY')
 }
+
 
 module.exports = {
   createHive,
