bc25128ac4850bf986f8d418f940ac8033b2674b
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-05 11:50:57 +1200

    Fix package.json dev script

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,55 +1,42 @@
 import React from 'react'
-// import request from 'superagent'
-
-// class App extends React.Component {
-    
-//     constructor(props) {
-//         super(props)
-//     }
-
-//     render() {
-//         return (
-//             <React.Fragment>
-//                 <h1>React development has begun!</h1>
-//                 <div id='indexHeader'>
-//                     <h1>Hivemind</h1>
-//                 </div>
-    
-//             {/* <form action="/ask" method='post'>
-//                 <label for="question">
-//                     <input type="text" name="name" value="What do you want to ask?">
-//                 </label>
-//                     <input type="submit" name="" value="Create a hive">
-//             </form> */}
-//             </React.Fragment>
-//                     )
-//                   }
-//     }
-             
 
 import { getFruits } from '../apiClient'
 
 class App extends React.Component {
-  state = {
-    fruits: []
+  constructor(props) {
+    super(props)
+    this.state = {
+      fruits: [],
+      name: 'What do you want to ask?',
+    }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     getFruits()
       .then(fruits => {
-        this.setState({fruits})
+        this.setState({ fruits })
       })
   }
 
-  render () {
+  render() {
     return (
       <div className='app'>
-        <h1>Fullstack Boilerplate</h1>
         <ul>
           {this.state.fruits.map(fruit => (
             <li key={fruit}>{fruit}</li>
           ))}
         </ul>
+        <div id='indexHeader'>
+          <h1>Hivemind</h1>
+        </div>
+        <div className="form">
+          <form action="/ask" method='post'>
+            <label for="question">
+              <input type="text" name="name" value={this.state.name}></input>
+            </label>
+            <input type="submit" name="" value="Create a hive"></input>
+          </form>
+        </div>
       </div>
     )
   }
