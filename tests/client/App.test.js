99c7846557d4d72589ae9065ed50ce9222fe2d7d
noemie-wandering-anemone <43393997+noemie-wandering-anemone@users.noreply.github.com>
2019-09-11 16:13:24 +1200

    Test react routes

--- tests/client/App.test.js
+++ tests/client/App.test.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Enzyme, {shallow, render, mount} from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import './setup-env'
+import { Route, Link, MemoryRouter } from "react-router-dom";
+// import './setup-env'
 
-import App from '../../client/components/App'
-App.prototype.componentDidMount = () => {}
+import App, {Home, Form, Hive} from '../../client/components/App'
+App.prototype.componentDidMount = () => { }
+
+import Buzzes from '../../client/components/Buzzes'
+Buzzes.prototype.componentDidMount = () => { }
 
 Enzyme.configure({adapter: new Adapter()})
 
@@ -18,14 +22,43 @@ test('<App> root has className of app', () => {
   expect(root.length).toBe(1)
 })
 
-test('page header includes fruit', () => {
+test('page header includes Hivemind', () => {
   const wrapper = render(<App />)
   const h1 = wrapper.find('h1')
-  expect(h1.text()).toMatch(/Fruit/)
+  expect(h1.text()).toMatch('Hivemind')
 })
 
-test('renders an <li> for each fruit', () => {
-  const wrapper = mount(<App />)
-  wrapper.setState({fruits: ['orange', 'persimmons', 'kiwi fruit']})
+let pathMap = {};
+describe('routes: paths point to the right component', () => {
+  beforeAll(() => {
+    const component = shallow(<App />);
+    pathMap = component.find(Route).reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+      }, {});
+  })
+  test('should show Home component for / router (getting array of routes)', () => {
+    expect(pathMap['/'].Function).toBe(Home);
+  })
+  test('should show Form component for /form router', () => {
+    expect(pathMap['/form'].Function).toBe(Form);
+  })
+  test('should show Hive for /hive/:id router', () => {
+    expect(pathMap['/hive/:id'].Function).toBe(Hive);
+  })
+})
+
+test('home has action button', () => {
+  const wrapper = render(<App />)
+  const button = wrapper.find('button')
+  expect(button.text()).toMatch('Create a hive')
+})
+
+
+
+test('renders an <li> for each buzz', () => {
+  const wrapper = mount(<Buzzes />)
+  wrapper.setState({buzzes: ['Buzz1', 'Buzz2', 'Buzz3']})
   expect(wrapper.find('li').length).toBe(3)
 })
