de32a4838905f43ae13b7317b4f87b6d3bacb9a5
Noemie <wandering_aneone@protonmail.com>
2019-11-29 14:27:19 +1300

    Fix merging conflict

--- client/index.js
+++ client/index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import {launchApp} from './app.js'
-
-document.addEventListener('DOMContentLoaded', launchApp)
-=======
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -13,4 +8,3 @@ document.addEventListener('DOMContentLoaded', () => {
     <App />,
     document.getElementById('app'))
 })
->>>>>>> a0e9d2e9c4ec52ca12da30a50fb689b4ced55b9a
